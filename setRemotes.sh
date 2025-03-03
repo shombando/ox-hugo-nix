a6eb542308d232bf17fd12884372e24337dfd175
Shom Bandopadhaya <shom@bandopadhaya.com>
2025-03-03 11:42:44 -0600

    Switching to Codeberg as primary

---
+++ setRemotes.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+set -eu
+git remote set-url --push origin --add git@codeberg.org:shom/ox-hugo-nix.git 
+git remote set-url --push origin --add git@git.sr.ht:~shom/ox-hugo-nix
+git remote set-url --push origin --add git@github.com:shombando/ox-hugo-nix.git
